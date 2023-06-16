251b7668802fc030259248389e875af2cf146544
Wonderbred <123215943+ElPasoJames@users.noreply.github.com>
2023-06-15 18:05:55 -0600

    Update core.py

--- core.py
+++ core.py
@@ -1,5 +1,6 @@
 import openai
 import os
+import requests
 
 API_KEY = 'XXXXXXX'
 openai.api_key = API_KEY
@@ -17,9 +18,15 @@ def chatgpt_conversation(conversation_log):
     })
     return conversation_log
 
+# Get the HTML code from the dashboard.html file
+response = requests.get('/templates/dashboard.html')
+
 conversations = []
 # system, user, assistant
-conversations.append({'role': 'system', 'content': 'Please read (input 1) and create me 5 short SEO keywords around (‘input 2) located in (input 3)})
+conversations['input1'] = input1
+conversations['input2'] = input2
+conversations['input3'] = input3
+conversations.append({'role': 'system', 'content': 'Please read ['input1'] and create me 5 short SEO keywords around ['input2'] located in ['input3']})
 conversations = chatgpt_conversation(conversations)
 print('{0}: {1}\n'.format(conversations[-1]['role'].strip(), conversations[-1]['content'].strip()))
 
